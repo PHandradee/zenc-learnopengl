8aa215a91f9ffc0645a8fbb02ea42ed5d81404aa
Pedro H. Andrade <63529201+PHandradee@users.noreply.github.com>
2026-04-14 20:29:20 -0300

    Implement 1.getting_started/4.6.textures_exercise4/textures_exercise4

---
+++ shaders/4.6.textures_exercise4/4.6.shader.fs
@@ -0,0 +1,13 @@
+    #version 330 core 
+    out vec4 FragColor;
+
+    in vec3 ourColor;
+    in vec2 TexCoord;
+
+    uniform sampler2D texture01;
+    uniform sampler2D texture02;
+    uniform float mixValue;
+
+    void main() { 
+    FragColor = mix(texture(texture01, TexCoord),texture(texture02,TexCoord),mixValue);
+    };
